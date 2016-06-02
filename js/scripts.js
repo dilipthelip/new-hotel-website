a15fa8a039d2f87068ce298da60267c69a22e70e
Toniokovalski <toniokovalski@gmail.com>
2016-06-02 19:29:14 +0300

    finished main body.started nes sliders.

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+(function($){
+    $(document).ready(function(){
+        $( ".menu-toggle" ).click(function() {
+            $(".menu").slideToggle( "fast" );
+        });
+
+        $( ".magnifier" ).click(function() {
+            $(".search").slideToggle("fast").focus();
+        });
+
+        $(".search").blur(function () {
+            $(".search").slideToggle("fast");
+        });
+
+        $( ".menu ul li .triangle-wrapper" ).click(function() {
+            $(this).find('span').toggleClass( "triangle-bottom" );
+            $(this).parent().find(".sub-menu").slideToggle( "fast" );
+        });
+
+        $('.flexslider').flexslider({
+            animation: "fade",
+            slideshow: true
+        });
+
+        $('.flexslider-2').flexslider({
+            animation: "slide"
+        });
+
+
+        $(function() {
+            var $dp = $( ".datepicker" );
+            $dp.datepicker({
+                showOtherMonths: true,
+                dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                dateFormat:"dd/mm/yy"
+            });
+
+            $dp.datepicker( "setDate" , "0");
+        });
+
+        $(".services").hover(function () {
+            $(this).addClass("active-div")
+        },
+        function () {
+            $(this).removeClass("active-div")
+        })
+    });
+})(jQuery);
