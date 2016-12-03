ee3032f9a1d2d19b8d204fca43a427070d97858b
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2016-12-03 16:04:16 -0600

    add the Staff and Wages section

--- js/app.js
+++ js/app.js
@@ -3,5 +3,15 @@ $(document).ready(function(){
 
   $("#loadHtml").load("header1.html");
 
+  // function orgChartClick(){
+  //   window.open("OrgChart/example/example.html","popup","height=1500px,width=1500px");
+  // //  window.open(url, windowName, "height=200,width=200");
+  // }
 
 });
+
+function orgChartClick(){
+  
+  window.open("OrgChart/example/example.html","popup","height=1500px,width=1500px");
+//  window.open(url, windowName, "height=200,width=200");
+}
