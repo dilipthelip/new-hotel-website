b2c2a74351b77079fb2adddc4880d904fca99d43
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2016-12-06 19:01:53 -0600

    added entertainment page

--- js/app.js
+++ js/app.js
@@ -3,15 +3,36 @@ $(document).ready(function(){
 
   $("#loadHtml").load("header1.html");
 
+  var mesage = "This is a prototype. Hold On Guys, Wait until the Website goes live.";
   // function orgChartClick(){
   //   window.open("OrgChart/example/example.html","popup","height=1500px,width=1500px");
   // //  window.open(url, windowName, "height=200,width=200");
   // }
 
+  $("#cirque").click(function(){
+      alert(mesage);
+  });
+
+  $("#kevin").click(function(){
+      alert(mesage);
+  });
+
+  $("#belly").click(function(){
+      alert(mesage);
+  });
+
+  $("#magic").click(function(){
+      alert(mesage);
+  });
+
+  $("#magic").click(function(){
+      alert(mesage);
+  });
+
 });
 
 function orgChartClick(){
-  
+
   window.open("OrgChart/example/example.html","popup","height=1500px,width=1500px");
 //  window.open(url, windowName, "height=200,width=200");
 }
